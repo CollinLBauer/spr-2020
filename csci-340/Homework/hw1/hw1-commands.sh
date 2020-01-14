6ccbda60d58534f3eeebfb7991e4e3e5c0566656
Collin <collinlbauer@gmail.com>
2020-01-14 08:19:01 -0500

    formatting

--- csci-340/Homework/hw1/hw1-commands.sh
+++ csci-340/Homework/hw1/hw1-commands.sh
@@ -18,24 +18,34 @@ OUT_FILE=$1
 rm $OUT_FILE
 touch $OUT_FILE
 
+
 # header
 printf "Collin Bauer\nCSCI 340 - Operating Systems\nDr. Crosby\nSpring 2020\n\n" >> $OUT_FILE
 echo "Note that, while I do have a Ubuntu VM on my Windows laptop, I also have a Linux laptop." >> $OUT_FILE
 echo "I plan to do most coding from my Linux laptop, but test sensitive code on the VM." >> $OUT_FILE
-printf "\nThe following output is from my Linux laptop.\n" >> $OUT_FILE
+printf "\nThe following output is from my Linux laptop.\n\n\n" >> $OUT_FILE
+
 
 # 1-a) Commands from homework instructions
-printf "\n====uname output====\n" >> $OUT_FILE
+echo "====uname output====" >> $OUT_FILE
 uname -a >> $OUT_FILE
-printf "\n====ps output====\n" >> $OUT_FILE
+printf "\n\n" >> $OUT_FILE
+
+echo "====ps output====" >> $OUT_FILE
 ps -al >> $OUT_FILE
-printf "\n====cat meminfo output====\n" >> $OUT_FILE
+printf "\n\n" >> $OUT_FILE
+
+echo "====cat meminfo output====" >> $OUT_FILE
 cat /proc/meminfo >> $OUT_FILE
-printf "\n====cat cpuinfo output====\n" >>$OUT_FILE
+printf "\n\n" >> $OUT_FILE
+
+echo "====cat cpuinfo output====" >>$OUT_FILE
 cat /proc/cpuinfo >> $OUT_FILE
+printf "\n" >> $OUT_FILE
+
 
 # 1-b) What these commands do
-printf "\n====Explanations of uname and ps commands====\n" >> $OUT_FILE
+echo "====Explanations of uname and ps commands====" >> $OUT_FILE
 # uname
 echo "uname (short for username) prints the user information from the system and related data, such as OS version, information, and time the command was run" >> $OUT_FILE
 # ps
@@ -43,6 +53,15 @@ echo "ps (short for processes) outputs a list of currently running processes." >
 echo "  -l makes the output take a long-hand form, reporting more information like parent process ID and memory address." >> $OUT_FILE
 echo "  -a extends the list to include all running processes, not just those visible to the bash shell." >> $OUT_FILE
 echo "  -al combines the functionality of both above arguments." >> $OUT_FILE
+printf "\n\n" >> $OUT_FILE
+
+
+# 2-b) Git status from ostep (copied and pasted)
+echo "====Git status output====" >> $OUT_FILE
+echo "On branch master" >> $OUT_FILE
+echo "Your branch is up to date with 'origin/master'." >> $OUT_FILE
+echo >> $OUT_FILE
+echo "nothing to commit, working tree clean" >> $OUT_FILE
 
 printf "Done.\n"
 exit
