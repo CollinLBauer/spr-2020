95924b98bc979bad811335dd3ab124bcc1c2edb7
collinlbauer <collinlbauer@gmail.com>
2020-01-21 17:05:21 -0500

    function stubs

--- csci-345/Homework/hw1/cracking.py
+++ csci-345/Homework/hw1/cracking.py
@@ -1,7 +1,39 @@
 import hashlib
 import re
+import sys
+
+# a seven char word from /usr/share/dict/words which gets the first letter
+# capitalized and a 1-digit number appended
+def ruleA(password):
+    #TODO
+    pass
+
+# a ten digit password with at least one of the following special
+# characters in the beginning: *,~,!,#
+def ruleB(password):
+    #TODO
+    pass
+
+# a five char word from /usr/share/dict/words witht he letter 'a' in it which
+# gets replaced with the special character '@' and the character 'l' which is
+# substituted with th number '1'
+def ruleC(password):
+    #TODO
+    pass
+
+# any word that is made with digits up to 100 digits length
+def ruleD(password):
+    #TODO
+    pass
+
+# any number of chars single word from /usr/share/dict/words
+def ruleE(password):
+    #TODO
+    pass
+
 
 def main():
+    #TODO
     pass
 
 main()
