09b57508f453f39a7e34061b660df4aad7bc7522
osboxes.org <osboxes@osboxes.org>
2020-01-28 20:04:39 -0500

    Rule 3 and 4 donegit add crackingIL.py ! Wooogit add crackingIL.py !

--- csci-345/Homework/hw1/crackingIL.py
+++ csci-345/Homework/hw1/crackingIL.py
@@ -41,30 +41,67 @@ def ruleC(inFile,dictPath="/usr/share/dict/words"):
     dict.seek(0);
 
     for word in dict:
+        ## Remove new line character, so that it really checks for words that are length 5
         word = word.strip("\n")
+        ## This rule only cares about words of length 5 and including the letter
+        ## a.  Check for these conditions.
         if(len(word) == 5 and ('a' in word or 'A' in word)):
+            ## Create letter replacements
             word = word.replace('a', '@')
             word = word.replace('A','@')
             word = word.replace('l','1')
             word = word.replace('L','1')
             hash = hashlib.sha256(word.encode("utf-8")).hexdigest()
 
+            ## This if statement only checks for uppercase passwords
             if(compareHashes(inFile, hash)):
                 return word, hash
+
+            ## Need to lowercase words to check for that instance
             word = word.lower()
             hash = hashlib.sha256(word.encode("utf-8")).hexdigest()
 
+            ## This if statement only checks for lowercase passwords
             if(compareHashes(inFile, hash)):
                 return word, hash
 
+    ## If no matches are found, return values of -1, for main to manage
     return "-1", "-1"
     dict.close()
 
 
 # any word that is made with digits up to 7 digits length
 def ruleD(inFile):
-    #TODO
-    pass
+    for x in range(10000000):
+        word = "{:01}".format(x)
+        hash = hashlib.sha256(word.encode("utf-8")).hexdigest()
+        if(compareHashes(inFile, hash)):
+            return word, hash
+        word = "{:02}".format(x)
+        hash = hashlib.sha256(word.encode("utf-8")).hexdigest()
+        if(compareHashes(inFile, hash)):
+            return word, hash
+        word = "{:03}".format(x)
+        hash = hashlib.sha256(word.encode("utf-8")).hexdigest()
+        if(compareHashes(inFile, hash)):
+            return word, hash
+        word = "{:04}".format(x)
+        hash = hashlib.sha256(word.encode("utf-8")).hexdigest()
+        if(compareHashes(inFile, hash)):
+            return word, hash
+        word = "{:05}".format(x)
+        hash = hashlib.sha256(word.encode("utf-8")).hexdigest()
+        if(compareHashes(inFile, hash)):
+            return word, hash
+        word = "{:06}".format(x)
+        hash = hashlib.sha256(word.encode("utf-8")).hexdigest()
+        if(compareHashes(inFile, hash)):
+            return word, hash
+        word = "{:07}".format(x)
+        hash = hashlib.sha256(word.encode("utf-8")).hexdigest()
+        if(compareHashes(inFile, hash)):
+            return word, hash
+    return "-1", "-1"
 
 # any number of chars single word from /usr/share/dict/words
 def ruleE(inFile,dictPath="/usr/share/dict/words"):
@@ -80,7 +117,7 @@ def main():
         return
 
     hashFile = open(sys.argv[1],"r")
-    password, hash = ruleC(hashFile)
+    password, hash = ruleD(hashFile)
     print("password: " + password + "\nhash: " + hash)
 
 
