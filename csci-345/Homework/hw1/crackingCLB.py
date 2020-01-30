3a94b5213d87e57188955a229b42b13289ede4e4
osboxes.org <osboxes@osboxes.org>
2020-01-30 17:34:27 -0500

    Every Rule

--- csci-345/Homework/hw1/crackingCLB.py
+++ csci-345/Homework/hw1/crackingCLB.py
@@ -14,7 +14,7 @@ def compareHashes(hashFile, testHash):
         hashList = hash.split(":")
         if(hashList[1] == testHash):
             return True
-    return False            
+    return False
 
 # a seven char word from /usr/share/dict/words which gets the first letter
 # capitalized and a 1-digit number appended
@@ -30,25 +30,25 @@ def ruleB(inFile):
     spec = ["*","~","!","#"]
     for i in spec:
         for num in range(10000): # 1 special character, 4 numbers
-            word = "{}{:05}".format(i,num)
+            word = "{}{:04}".format(i,num)
             hash = hashlib.sha256(word.encode("utf-8")).hexdigest()
             if compareHashes(inFile,hash):
                 return word, hash
         for j in spec:
             for num in range(1000): # 2 special characters, 3 numbers
-                word = "{}{}{:05}".format(i,j,num)
+                word = "{}{}{:03}".format(i,j,num)
                 hash = hashlib.sha256(word.encode("utf-8")).hexdigest()
                 if compareHashes(inFile,hash):
                     return word, hash
             for k in spec:
                 for num in range(100): # 3 special characters, 2 numbers
-                    word = "{}{}{}{:05}".format(i,j,k,num)
+                    word = "{}{}{}{:02}".format(i,j,k,num)
                     hash = hashlib.sha256(word.encode("utf-8")).hexdigest()
                     if compareHashes(inFile,hash):
                         return word, hash
                 for l in spec:
                     for num in range(10): # 4 special characters, 1 number
-                        word = "{}{}{}{}{:05}".format(i,j,k,l,num)
+                        word = "{}{}{}{}{:01}".format(i,j,k,l,num)
                         hash = hashlib.sha256(word.encode("utf-8")).hexdigest()
                         if compareHashes(inFile,hash):
                             return word, hash
@@ -59,7 +59,7 @@ def ruleB(inFile):
                             return word, hash
 
 
-    return -1, -1
+    return "-1", "-1"
 
 # a five char word from /usr/share/dict/words witht he letter 'a' in it which
 # gets replaced with the special character '@' and the character 'l' which is
@@ -85,7 +85,7 @@ def ruleE(inFile,dictPath="/usr/share/dict/words"):
 
 def main():
     args = sys.argv
-    if len(args) <= 1:
+    if len(args) != 2:
         print("Argument expected.")
         return
 
