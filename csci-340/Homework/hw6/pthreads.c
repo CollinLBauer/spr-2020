f32271fbd8b424e4c862cf52f50b1bcd9b4c9a82
collinlbauer <collinlbauer@gmail.com>
2020-03-01 19:19:35 -0500

    progress

--- csci-340/Homework/hw6/pthreads.c
+++ csci-340/Homework/hw6/pthreads.c
@@ -2,15 +2,28 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-int main(int argc, int *argv){
-    if (argv != 1){
-        printf("%d arguments found; expected %d.", argc, 2);
+void *threadFun(int *threadNum){
+    //TODO
+    printf("In thread %d\n", *threadNum);
+    return NULL;
+}
+
+int main(int argc, char **argv){
+    if (argc != 3){
+        printf("%d arguments found; expected %d.", argc-1, 2);
         printf("Usage: ./pthreads <thread-num> <len-string>\n");
         exit(1);
     }
-    int numThreads = argv[0];
-    int strLen = argv[1];
 
+    int numThreads = atoi(argv[1]);
+    //int strLen = atoi(argv[2]);
 
+    pthread_t *threads = malloc(numThreads * sizeof(pthread_t));
+    for (int i = 0; i < numThreads; i++){
+        printf("Starting thread %d...\n", i);
+        pthread_create(&threads[i], NULL, *threadFun, &i);
+    }
+
+    printf("Done\n");
     exit(0);
 }
