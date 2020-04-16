8798508d045bf280022f03862441a0de6ff9dfcf
Collin <collinlbauer@gmail.com>
2020-04-16 00:57:28 -0400

    hw10 final?

--- csci-340/Homework/hw10/src/hw10.c
+++ csci-340/Homework/hw10/src/hw10.c
@@ -6,38 +6,39 @@
 #include <memory.h>
 
 int main(int argc, char **argv) {
+    // assert usage
     if (argc != 2){
         fprintf(stderr, "Usage: ./hw10 <file>\n");
         exit(1);
     }
-
+    // attempt to open file
     int infile = open(argv[1], O_RDONLY);
     if (infile == -1){
         fprintf(stderr, "File %s not found.\n", argv[1]);
         exit(1);
     }
 
+    // read header data
     char header[5] = {0};
-    read(infile, header, 4);
-
-    
     int off_size;
-    read(infile, &off_size, 4);
-
     int offset_arr[off_size];
+    read(infile, header, 4);
+    read(infile, &off_size, 4);
     for (int i = 0; i < off_size; i++) {
         read(infile, &offset_arr[i], 4);
     }
 
-    // debug prints
+    /* debug prints
     printf("%s\n", header);
     printf("%d\n{", off_size);
     for (int i = 0; i < off_size; i++) {
         printf("%d, ", offset_arr[i]);
     }
     printf("}\n");
-    // end debug prints
+    end debug prints */
 
+    // read and print each line from file,
+    // according to offset array and line length
     int ln_length;
     char buffer[255];
     for (int i = 0; i < off_size; i++) {
@@ -45,10 +46,9 @@ int main(int argc, char **argv) {
         lseek(infile, offset_arr[i], SEEK_SET);
         read(infile, &ln_length, 4);
         read(infile, buffer, ln_length - 4);
-        printf("%d: [%s]\n", ln_length, buffer);
-
+        //printf("%d: ", ln_length); //debug
+        printf("%s", buffer);
     }
 
-    
     exit(0);
 }
